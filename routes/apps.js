2b01c2c2cb077829ac41412503eb79f082d3ae92
zoops <jhbaik@gmail.com>
2018-07-11 04:54:39 +0900

    T01

--- routes/apps.js
+++ routes/apps.js
@@ -8,21 +8,57 @@ router.get('/', function(req, res, next) {
   res.send('invalid request uri');
 });
 
-router.get('/getRnDInfos', function(req, res, next) {
+function isFiltered(it, kword){
+  var res =  (it.title.toLowerCase().indexOf(kword) >= 0 || it.cat.toLowerCase().indexOf(kword) >= 0);
+  if (res == false)
+    res = it.asocicateWord.some(e => (e.toLowerCase().indexOf(kword) >= 0) );
+  return res;
+};
+
+router.get('/issue/getRnDInfos', function(req, res, next) {
   res.send( { "result": true,
               "msgCode": "100",
               "message": "요청 작업 성공",
               "list" : datas } );
 });
 
-function isFiltered(it, kword){
-  var res =  (it.title.toLowerCase().indexOf(kword) >= 0 || it.cat.toLowerCase().indexOf(kword) >= 0);
-  if (res == false)
-    res = it.asocicateWord.some(e => (e.toLowerCase().indexOf(kword) >= 0) );
-  return res;
-}
+router.post('/issue/getRnDInfos', function(req, res, next) {
+  var filtered = datas;
+  if (req.body.kword) 
+    filtered = datas.filter( e => isFiltered(e, req.body.kword.toLowerCase()) );
+  
+  res.send( { "result": true,
+              "msgCode": "100",
+              "message": "요청 작업 성공",
+              "list" : filtered } );
+});
 
-router.post('/getRnDInfos', function(req, res, next) {
+router.get('/category/getRnDInfos', function(req, res, next) {
+  res.send( { "result": true,
+              "msgCode": "100",
+              "message": "요청 작업 성공",
+              "list" : datas } );
+});
+
+router.post('/category/getRnDInfos', function(req, res, next) {
+  var filtered = datas;
+  if (req.body.kword) 
+    filtered = datas.filter( e => isFiltered(e, req.body.kword.toLowerCase()) );
+  
+  res.send( { "result": true,
+              "msgCode": "100",
+              "message": "요청 작업 성공",
+              "list" : filtered } );
+});
+
+router.get('/peer/getRnDInfos', function(req, res, next) {
+  res.send( { "result": true,
+              "msgCode": "100",
+              "message": "요청 작업 성공",
+              "list" : datas } );
+});
+
+router.post('/peer/getRnDInfos', function(req, res, next) {
   var filtered = datas;
   if (req.body.kword) 
     filtered = datas.filter( e => isFiltered(e, req.body.kword.toLowerCase()) );
